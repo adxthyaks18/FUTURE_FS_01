bd997abdacb2f3f696574aeb33e86f681d013506
Adithya K S <adxthyaks662@gmail.com>
2026-06-02 19:26:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", () => {
+    
+    
+    const themeToggleBtn = document.getElementById("theme-toggle-btn");
+    const bodyElement = document.body;
+
+    themeToggleBtn.addEventListener("click", () => {
+        if (bodyElement.classList.contains("dark-theme")) {
+            bodyElement.classList.remove("dark-theme");
+            bodyElement.classList.add("light-theme");
+            themeToggleBtn.innerHTML = `<i class="fas fa-moon"></i>`;
+        } else {
+            bodyElement.classList.remove("light-theme");
+            bodyElement.classList.add("dark-theme");
+            themeToggleBtn.innerHTML = `<i class="fas fa-sun"></i>`;
+        }
+    });
+const ContactForm = document.getElementById("portfolio-Contact-form");
+    const fbSuccessMsg = document.getElementById("fb-success-msg");
+
+    feedbackForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+        
+        fbSuccessMsg.style.display = "block";
+        feedbackForm.reset();
+
+        setTimeout(() => {
+            fbSuccessMsg.style.display = "none";
+        }, 4000);
+    });
+});
